65f4c8481aee46e7f03f8fc466798479bb569c58
bilal iken <bilal11iken@gmail.com>
2026-05-08 00:41:00 +0100

    add

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import {connectdb} from "./lib/db.js"
+// import {connectdb} from "./lib/db.js"
 import cors from "cors"
 import gatewayRoutes from "./routes/gateway.routes.js";
 
@@ -20,7 +20,7 @@ const PORT = process.env.PORT
 
 app.use("/", gatewayRoutes);
 
-connectdb()
+// connectdb()
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`)
